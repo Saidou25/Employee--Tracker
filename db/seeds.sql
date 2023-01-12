d84e092ee119f716ee590bd8ef315439d3c7da1e
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-01-12 17:19:59 -0500

    organized departments, roles and names.

---
+++ db/seeds.sql
@@ -0,0 +1,17 @@
+departments
+
+Engineering
+     Lead Engineering
+         Ashley Rodriguez
+     Software Engineer
+         Kevin Tupik
+Finance
+     Account Manageer
+         Kunal Sigh
+     Acountant
+         Nalia Brown
+Legal
+     Legal Team Lead
+         Srah Lourd
+         Lawyer
+         Tom Allen
