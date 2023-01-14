cf2ee06274ececded26f26f81de574fd93b0d4a4
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-01-14 16:50:57 -0500

    auto incremented ids for role and employee, now tables show automated attribution of ids for primary keys

--- db/seeds.sql
+++ db/seeds.sql
@@ -7,20 +7,20 @@ USE employee_db;
         ("Finance"),
         ("Legal");
 
-        INSERT INTO role (id, title, salary)
+        INSERT INTO role (title, salary)
         VALUES
-         (7, "Lead Engineering", 150000),
-         (8, "Software Engineer", 120000),
-         (9, "Account Manageer", 160000),
-         (10, "Acountant", 12500),
-         (11, "Legal Team Lead", 250000),
-          (12, "Lawyer", 190000);
+         ("Lead Engineering", 150000),
+         ("Software Engineer", 120000),
+         ("Account Manageer", 160000),
+         ("Acountant", 12500),
+         ("Legal Team Lead", 250000),
+          ("Lawyer", 190000);
 
-            INSERT INTO employee (id, first_name, last_name)
+            INSERT INTO employee (first_name, last_name)
         VALUES
-             (1, "Ashley", "Rodriguez"),
-             (2,"Kevin", "Tupik"),
-             (3,"Kunal", "Sigh"),
-             (4,"Nalia", "Brown"),
-              (5,"Sarah", "Lourd"),
-              (6,"Tom", "Allen");
+             ("Ashley", "Rodriguez"),
+             ("Kevin", "Tupik"),
+             ("Kunal", "Sigh"),
+             ("Nalia", "Brown"),
+              ("Sarah", "Lourd"),
+              ("Tom", "Allen");
