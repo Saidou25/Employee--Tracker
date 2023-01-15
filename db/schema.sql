c932823ecf2a334d0e7de1e49f819bfe184eaded
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-01-15 14:30:28 -0500

    help

--- db/schema.sql
+++ db/schema.sql
@@ -10,10 +10,10 @@ CREATE TABLE departments (
     );
 
 CREATE TABLE roles (
-        id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        id INT NOT NULL PRIMARY KEY,
         title VARCHAR(30) NOT NULL,
         salary DECIMAL NOT NULL,
-        department_id INT,
+        department_id INT,/* not null isn't here so we can do associations with other tables later on*/
         FOREIGN KEY (department_id) REFERENCES departments(id)
     );
 
@@ -21,19 +21,25 @@ CREATE TABLE employees (
         id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
         first_name VARCHAR(30) NOT NULL,
         last_name VARCHAR(30) NOT NULL,
-        role_id INT,
-        CONSTRAINT employees_ibfk_1
-        FOREIGN KEY (role_id) REFERENCES roles(id)
+        department_id INT,
+        role_id INT,/* not null isn't here so we can do associations with other tables later on*/
+        FOREIGN KEY (role_id) REFERENCES roles(id), /*ON DELETE SET NULL*/
+        FOREIGN KEY ( department_id) REFERENCES departments(id)
+
         /* manage_id INT, */
        
     );
 
-    /* SELECT 
-     id,
+     SELECT 
+     employees.id,
+     name,
      first_name,
      last_name,
+     title,
+     salary
     FROM employees 
-    JOIN roles ON role_id = roles.id */
+    JOIN roles ON employees.role_id = roles.id
+    JOIN departments ON employees.department_id = departments.id;
     /* JOIN departments ON  */
 
 SELECT DATABASE();
