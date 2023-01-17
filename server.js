43c9168f30f2b172a06ab73cf81b340b98128a88
Saidou25 <115666389+Saidou25@users.noreply.github.com>
2023-01-16 22:45:11 -0500

    reconstructed the server.js as wasn '

--- server.js
+++ server.js
@@ -6,68 +6,129 @@ require('console.table');
 const db = mysql.createConnection({
     user: "root",
     database: "employee_db",
-  
+
 });
 
-// db.query('SELECT * FROM departments', (err, departments) => {
-//     console.table(departments);
-// });
-
-// WHEN I start the application
-// THEN I am presented with the following options:
-
-// view all departments
-// view all roles
-// view all employees
-const viewTables = () => {
+const init = () => {
     prompt(
         {
             type: 'list',
-            message: 'Wich list would you like to see ?',
-            name: 'question',
-            choices: ['Departments', 'Roles', 'Employees'],
+            message: 'What would you like to do ?',
+            name: 'init',
+            choices: ['View all Departments', 'View all Roles', 'View all Employees',
+             'Add a Department',' Add a Role', 'Add an Employee', 'Update an Employee Role'],
         },
     )
         .then((answer) => {
-            showTables(answer);
+            if  (answer.init.includes('Departments')) { 
+                let result = 'Departments';
+                showTables(result);
+            }
+               else if  (answer.init.includes('Roles')) { 
+                let result = 'Roles';
+                showTables(result);
+            }
+              else if  (answer.init.includes('Employees')) { 
+                let result = 'Employees';
+                showTables(result);
+            };
+
+        })
+        .then((answer) => {
+            if  (answer.init.includes('Add a Department')) { 
+                let result = 'Departments';
+                showTables('great');
+            }
         })
         .catch(err => console.log(err));
 };
 
-const showTables = (answer) => {
-    const showTable = 'SELECT * FROM ' + 'view' + answer.question;
-    console.log(showTable);
+init();
+
+const showTables = (result) => {
+    const showTable = 'SELECT * FROM ' + 'view' + result;
     db.query(showTable, (err, showTable) => {
-        console.table(showTable);
+        console.table(showTable);      
     });
 }
 
-viewTables();
-// WHEN I start the application
-// THEN I am presented with the following options:
+//         .then((answer) => {
+//             const toAdd = 'view' + answer.question;
+//             db.query('INSERT INTO ?? SET ?', [viewDepartments, name],(err) => {
+//                 if (err) return console.error(err);
+//              console.table('sucess');     
+//             })
+//             .catch (err => console.log(err));
+//         })
+//     }
 
+
+
+// db.query('SELECT * FROM departments', (err, departments) => {
+//     console.table(departments);
+// });
+// .then((answer) => {
+//     const toAdd = 'view' + answer.question;
+//     db.query('INSERT INTO ?? SET ?', [viewDepartments, name](err) => {
+//      console.table(showTable);
+
+     
+
+// view all departments
+// view all roles
+// view all employees
 // add a department
 //  add a role
 //   add an employee
+// update an employee role
 
-// const addSomething = () => {
-
-//     prompt(
-//         {
-//             type: 'rawlist',
-//             message: 'Would you like to add?',
-//             name: 'question',
-//             choices: ['departments', 'roles', 'employees'],
-//         },
-//     )
-//         .then((answer) => {
-//             showTables(answer);
-//             return (answer);
-//         })
-//         .catch(err => console.log(err));
-
-// };
-
-
-
-
+// prompt([
+//     {
+//         type: 'rawlist',
+//         message: 'Would you like to add a new',
+//         name: 'all departments',
+//         choices: ['Department ?', 'Role ?', 'Employee ?'],
+//     },
+//     {
+//         type: 'rawlist',
+//         message: 'Enter the name of?',
+//         name: 'all roles',
+//         choices: ['New department', 'New role', 'New employee'],
+//     },
+//     {
+//         type: 'rawlist',
+//         message: 'Would you like to add a new',
+//         name: 'all departments',
+//         choices: ['Department ?', 'Role ?', 'Employee ?'],
+//     },
+//     {
+//         type: 'rawlist',
+//         message: 'Enter the name of?',
+//         name: 'all roles',
+//         choices: ['New department', 'New role', 'New employee'],
+//     },
+//     {
+//         type: 'rawlist',
+//         message: 'Would you like to add a new',
+//         name: 'all departments',
+//         choices: ['Department ?', 'Role ?', 'Employee ?'],
+//     },
+//     {
+//         type: 'rawlist',
+//         message: 'Enter the name of?',
+//         name: 'all roles',
+//         choices: ['New department', 'New role', 'New employee'],
+//     },
+//     {
+//         type: 'rawlist',
+//         message: 'Would you like to add a new',
+//         name: 'all departments',
+//         choices: ['Department ?', 'Role ?', 'Employee ?'],
+//     },
+//     {
+//         type: 'rawlist',
+//         message: 'Enter the name of?',
+//         name: 'all roles',
+//         choices: ['New department', 'New role', 'New employee'],
+//     },
+// ])
